df0dde70ef8a01961552acda06f73eda8d0dcdad
ashleyn <ashleynavo@googlemail.com>
2022-02-16 23:11:04 +0000

    logo added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,19 @@
 import React from "react";
 import "../styles/NavBar.css";
+// import logo from "../images/logo.png";
 
 export default function NavBar() {
   return (
     <div className="navbar">
-      <header>This is the NavBar</header>
+      <ul className="navbar-links">
+        <img
+          className="navbar-logo"
+          alt="surreal estate"
+          src="../images/logo.png"
+        />
+        <p className="navbar-links-item">View Properties</p>
+        <p className="navbar-links-item">Add a Property</p>
+      </ul>
     </div>
   );
 }
