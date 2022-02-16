765981ae63b598739fe61a34f6e92e24c81c1131
ashleyn <ashleynavo@googlemail.com>
2022-02-16 20:55:07 +0000

    NavBar added

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
+import React from "react";
 import "../styles/App.css";
+import NavBar from "./NavBar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">Surreal Estate</header>
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/NavBar.css";
+
+export default function NavBar() {
+  return (
+    <div className="navbar">
+      <header>This is the NavBar</header>
+    </div>
+  );
+}
